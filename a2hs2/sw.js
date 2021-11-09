af256dfb8bfed79fd84777b4775a72a6d8e58591
lazzzis <lazzzis@outlook.com>
2021-11-09 10:46:01 -0800

    a2hs2

--- a2hs2/sw.js
+++ a2hs2/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/fox1.jpg',
-      '/pwa-examples/a2hs/images/fox2.jpg',
-      '/pwa-examples/a2hs/images/fox3.jpg',
-      '/pwa-examples/a2hs/images/fox4.jpg',
+      '/pwa-examples/a2hs2/',
+      '/pwa-examples/a2hs2/index.html',
+      '/pwa-examples/a2hs2/index.js',
+      '/pwa-examples/a2hs2/style.css',
+      '/pwa-examples/a2hs2/images/fox1.jpg',
+      '/pwa-examples/a2hs2/images/fox2.jpg',
+      '/pwa-examples/a2hs2/images/fox3.jpg',
+      '/pwa-examples/a2hs2/images/fox4.jpg',
     ])),
   );
 });
